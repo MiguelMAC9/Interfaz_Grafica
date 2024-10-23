54c05751036d02f37263eeff7e6fe5be27317072
MiguelMAC9 <100552483+MiguelMAC9@users.noreply.github.com>
2024-10-22 20:22:37 -0600

    Interfaz_grafica
    Aquí podrán encontrar la interfaz grafica que hice para mi calificación de examen de base de datos. Esta interfaz para abrirla necesitan XAMPP Apache.

---
+++ Proyecto Final Base de datos/alquiler/Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+const base_url = "http://localhost/Proyecto%20Final%20Base%20de%20datos/alquiler//";
+const host = "localhost";
+const user = "root";
+const pass = "";
+const db = "venta_de_vehiculos";
+const charset = "charset=utf8";
+?>
